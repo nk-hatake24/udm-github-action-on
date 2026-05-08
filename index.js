892a58e1b4bbca5ac454a25068ebdde2795c4351
Nkenla Michel (mitcode) <nkenlamichel@gmail.com>
2026-05-08 16:51:49 +0100

    test file updates

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+const { add } = require('./math');
+
 console.log('hello world');
+console.log('Sum of 2 and 3 is:', add(2, 3));
---
+++ index.test.js
@@ -0,0 +1,5 @@
+// index.test.js
+test('A simple test to check if math works', () => {
+  const sum = 1 + 2;
+  expect(sum).toBe(3);
+});
